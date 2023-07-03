7bb19e5716f48cf56b0d1cb20d377eed0c60dfea
Markello <jleta084@uottawa.ca>
2023-07-03 00:46:44 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import Home from './components/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
